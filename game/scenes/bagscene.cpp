97518b89333d7b3f54a13c171e2dd9d2c959e379
xavi-b <developer@xavi-b.fr>
2024-11-27 19:56:14 +0100

    game: bagscene basics

--- game/scenes/bagscene.cpp
+++ game/scenes/bagscene.cpp
@@ -2,18 +2,98 @@
 
 BagScene::BagScene(SDL_Renderer* renderer) : Scene(renderer)
 {
+    for (size_t i = 0; i < NumberOfPockets; ++i)
+    {
+        SDL_Surface* surface = IMG_Load(("resources/Graphics/UI/Bag/bg_" + std::to_string(i + 1) + ".png").c_str());
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+        backgrounds.push_back({surface, texture});
+    }
+    for (size_t i = 0; i < NumberOfPockets; ++i)
+    {
+        SDL_Surface* surface = IMG_Load(("resources/Graphics/UI/Bag/bag_" + std::to_string(i + 1) + ".png").c_str());
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+        bags.push_back({surface, texture});
+    }
+
+    iconPocketSurface = IMG_Load("resources/Graphics/UI/Bag/icon_pocket.png");
+    iconPocketTexture = SDL_CreateTextureFromSurface(renderer, iconPocketSurface);
 }
 
 BagScene::~BagScene()
 {
+    for (auto& e : backgrounds)
+    {
+        SDL_DestroyTexture(e.second);
+        SDL_FreeSurface(e.first);
+    }
+    for (auto& e : bags)
+    {
+        SDL_DestroyTexture(e.second);
+        SDL_FreeSurface(e.first);
+    }
+
+    SDL_DestroyTexture(iconPocketTexture);
+    SDL_FreeSurface(iconPocketSurface);
 }
 
-void BagScene::update(Inputs const* /*inputs*/)
+void BagScene::update(Inputs const* inputs)
 {
+    if (inputs->B)
+    {
+        leave = true;
+        return;
+    }
+
+    if (inputs->left)
+    {
+        currentPocketIndex = (currentPocketIndex + NumberOfPockets - 1) % NumberOfPockets;
+        return;
+    }
+    else if (inputs->right)
+    {
+        currentPocketIndex = (currentPocketIndex + NumberOfPockets + 1) % NumberOfPockets;
+        return;
+    }
 }
 
-void BagScene::draw(Fps const* /*fps*/, RenderSizes /*rs*/)
+void BagScene::draw(Fps const* /*fps*/, RenderSizes rs)
 {
+    SDL_Surface* surface;
+    SDL_Texture* texture;
+
+    SDL_Rect srcRect;
+    SDL_Rect dstRect;
+
+    surface   = backgrounds[currentPocketIndex].first;
+    texture   = backgrounds[currentPocketIndex].second;
+    dstRect.x = 0;
+    dstRect.y = 0;
+    dstRect.w = surface->w * rs.ww / rs.aw;
+    dstRect.h = surface->h * rs.wh / rs.ah;
+    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+
+    surface   = bags[currentPocketIndex].first;
+    texture   = bags[currentPocketIndex].second;
+    dstRect.x = (184 - surface->w) * rs.ww / rs.aw / 2;
+    dstRect.y = (184 - surface->h) * rs.wh / rs.ah / 2;
+    dstRect.w = surface->w * rs.ww / rs.aw;
+    dstRect.h = surface->h * rs.wh / rs.ah;
+    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+
+    srcRect.x = 28 * currentPocketIndex;
+    srcRect.y = 0;
+    srcRect.w = 28;
+    srcRect.h = 28;
+    dstRect.x = (2 + 22 * currentPocketIndex) * rs.ww / rs.aw;
+    dstRect.y = 226 * rs.wh / rs.ah;
+    dstRect.w = 28 * rs.ww / rs.aw;
+    dstRect.h = 28 * rs.wh / rs.ah;
+    SDL_RenderCopy(renderer, iconPocketTexture, &srcRect, &dstRect);
+}
+
+bool BagScene::popScene() const
+{
+    return leave;
 }
 
 std::string BagScene::name()
--- game/scenes/bagscene.h
+++ game/scenes/bagscene.h
@@ -4,6 +4,7 @@
 #include "scene.h"
 
 #include <SDL_image.h>
+#include <vector>
 
 class BagScene : public Scene
 {
@@ -14,9 +15,22 @@ public:
     virtual void update(Inputs const* inputs) override;
     virtual void draw(Fps const* fps, RenderSizes rs) override;
 
+    virtual bool popScene() const override;
+
     virtual std::string name() override;
 
+    static constexpr size_t NumberOfPockets = 8;
+
 private:
+    std::vector<std::pair<SDL_Surface*, SDL_Texture*>> backgrounds;
+    std::vector<std::pair<SDL_Surface*, SDL_Texture*>> bags;
+
+    SDL_Surface* iconPocketSurface;
+    SDL_Texture* iconPocketTexture;
+
+    bool leave = false;
+
+    size_t currentPocketIndex = 0;
 };
 
 #endif // BAGSCENE_H
